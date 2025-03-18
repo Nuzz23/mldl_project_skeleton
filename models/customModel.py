ac167ffe3433f2d4df1bc8e295e43abe73be2396
Nunzio <s298825@studenti.polito.it>
2025-03-18 17:03:31 +0100

    pushami

--- models/customModel.py
+++ models/customModel.py
@@ -1,30 +1,28 @@
-from torch import nn
-# L'immagine è 224x224 e si passa da 3 canali rgb
+import torch
 
-# Define the custom neural network
-class CustomNet(nn.Module):
+class CustomNet(torch.nn.Module):
     def __init__(self):
         super(CustomNet, self).__init__()
         # Define layers of the neural network
-        self.linear_relu_stack = nn.Sequential(
-          nn.Conv2d(3, 16, kernel_size=3, padding=1, stride=2), # 112
-          nn.BatchNorm2d(num_features=16),
-          nn.LeakyReLU(),
-          nn.Conv2d(16, 32, kernel_size=3, padding=1, stride=2), # 56
-          nn.BatchNorm2d(num_features=32),
-          nn.LeakyReLU(),
-          nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=2), # 28
-          nn.BatchNorm2d(num_features=64),
-          nn.LeakyReLU(),
-          nn.Conv2d(64, 128, kernel_size=3, padding=1),
-          nn.BatchNorm2d(num_features=128),
-          nn.LeakyReLU(),
-          nn.Conv2d(128, 256, kernel_size=3, padding=1),
-          nn.BatchNorm2d(num_features=256),
-          nn.LeakyReLU(),
-          nn.AvgPool2d(4, 4), # Add a max pooling layer
-          nn.Flatten(),
-          nn.Linear(256*7**2, 200),  # 200 output classes
+        self.linear_relu_stack = torch.nn.Sequential(
+          torch.nn.Conv2d(3, 16, kernel_size=3, padding=1, stride=2), # 112
+          torch.nn.BatchNorm2d(num_features=16),
+          torch.nn.LeakyReLU(),
+          torch.nn.Conv2d(16, 32, kernel_size=3, padding=1, stride=2), # 56
+          torch.nn.BatchNorm2d(num_features=32),
+          torch.nn.LeakyReLU(),
+          torch.nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=2), # 28
+          torch.nn.BatchNorm2d(num_features=64),
+          torch.nn.LeakyReLU(),
+          torch.nn.Conv2d(64, 128, kernel_size=3, padding=1),
+          torch.nn.BatchNorm2d(num_features=128),
+          torch.nn.LeakyReLU(),
+          torch.nn.Conv2d(128, 256, kernel_size=3, padding=1),
+          torch.nn.BatchNorm2d(num_features=256),
+          torch.nn.LeakyReLU(),
+          torch.nn.AvgPool2d(4, 4), # Add a max pooling layer
+          torch.nn.Flatten(),
+          torch.nn.Linear(256*7**2, 200),  # 200 output classes
         )
 
     def forward(self, x):
