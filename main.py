1b26a698529df59148e5349b0279ce6257f41831
Nunzio <s298825@studenti.polito.it>
2025-03-18 18:18:38 +0100

    prova

---
+++ main.py
@@ -0,0 +1,31 @@
+from models.customModel import CustomNet
+import torch
+from train import train
+from eval import validate
+from data.dataManagment import get_data_loaders
+import wandb
+
+
+def main(config):
+    model = CustomNet().cuda()
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate, momentum=config.momentum)
+
+    best_acc = 0
+
+    train_loader, val_loader = get_data_loaders()
+
+    # Run the training process for {num_epochs} epochs
+    for epoch in range(1, 5 + 1):
+        train(epoch, model, train_loader, criterion, optimizer)
+
+        # At the end of each training iteration, perform a validation step
+        val_accuracy = validate(model, val_loader, criterion)
+        
+        # Best validation accuracy
+        best_acc = max(best_acc, val_accuracy)
+        
+        wandb.log({'val_accuracy': val_accuracy, "epoch": epoch})
+
+
+    print(f'Best validation accuracy: {best_acc:.2f}%')
